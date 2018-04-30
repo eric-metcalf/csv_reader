0fce822e4605919ab3f55e9c41bfb8b1b9a110db
eric-metcalf <eric_metcalf@brown.edu>
2018-04-30 18:46:06 -0400

    added csv reader and other stuff

---
+++ csv_reader_test.cpp
@@ -0,0 +1,22 @@
+//
+// Created by eric on 4/30/18.
+//
+
+
+
+/*
+ *
+ * This is the file that is going to test the csv reader
+ *
+ *
+ */
+
+#include <iostream>
+
+int main(){
+    /* this is a fucction that prints out hello world.
+     *
+     */
+    std::cout << "Hello World!!"<<std::endl;
+
+}
