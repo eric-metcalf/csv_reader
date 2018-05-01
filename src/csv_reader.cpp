6f8c288ad258d0eac7aeb26dc985669bcbf5bd76
eric-metcalf <eric_metcalf@brown.edu>
2018-04-30 20:26:25 -0400

    added csv_reader.cpp

---
+++ src/csv_reader.cpp
@@ -0,0 +1,45 @@
+
+//
+// Created by ehm216 on 4/30/18.
+//
+
+#ifndef PROJECT_CSV_READER_H
+#define PROJECT_CSV_READER_H
+
+
+#include <fstream>
+#include "csv_reader.h"
+
+class CSVReader{
+
+private:
+    //Properties of the csv
+    int number_atts;
+    bool openned_file = false;
+    // 1 for int, 0 for float,
+    vector<int> attribute_type;
+
+
+    vector<std::string> attribute_name;
+
+    int lineCount;
+
+    //path to file
+    std::string file_path;
+    ifstream csv;
+
+public:
+
+    CSVReader(std::vector<int> attribute_type, vector<std::string> attribute_name, std::string file_path);
+    std::vector<std::vector> getNextLine();
+    std::vector<std::vector> getLines(int num_lines);
+    int reset();
+    int seek(int line);
+    int open();
+    int close();
+};
+
+
+
+
+#endif //PROJECT_CSV_READER_H
